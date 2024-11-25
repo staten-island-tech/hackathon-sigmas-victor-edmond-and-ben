0b48814f829ef1565c9a4a08b1c063825edce558
Victor-K-19 <victork33@nycstudents.net>
2024-11-25 09:44:59 -0500

    65

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\edmond.zhao24\Documents\GitHub\hackathon-sigmas-victor-edmond-and-ben\venv
+set VIRTUAL_ENV=C:\Users\victor.khobot24\Documents\GitHub\hackathon-sigmas-victor-edmond-and-ben\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
